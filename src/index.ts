1aa2ab1b1edb6cb6f0b5e2f8e22e80180546b607
Oscar 8A <oscarochoacerrato@gmail.com>
2022-08-17 11:19:22 -0400

    beez in the trap

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-// your code here
+const button = document.getElementById("btn") as HTMLButtonElement;
+const input1 = document.getElementById("firstInput") as HTMLInputElement;
+const input2 = document.getElementById("secondInput") as HTMLInputElement;
+
+button!.addEventListener("click", function () {
+    console.log(prepend(input1!.value, input2!.value));
+});
+
+function prepend(numSpaces: number | string, inputString: string) {
+    let i = 0;
+    while (i < numSpaces){
+      inputString = " " + inputString;
+      i++;
+    }
+    return inputString;
+  }
